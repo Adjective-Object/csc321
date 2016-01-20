0b5da97a33400832a602b0a3e85e19fdfe44dc38
Adjective-Object <mhuan13@gmail.com>
2016-01-19 19:54:26 -0500

    basic knn implementation

--- a1/knn.py
+++ a1/knn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import sys
 
 from scipy.spatial.distance import euclidean
 from scipy.misc import imread
@@ -32,10 +33,7 @@ data = [
 #########################################
 
 def ilen(iterator):
-    ct = 0
-    for i in iterator:
-        ct += 1
-    return ct
+    return len(list(iterator))
 
 def verify_set(kind, *sets):
     """ verifies that the set given has the correct folder structure
@@ -54,34 +52,34 @@ def verify_set(kind, *sets):
 def counts_of(kind, *sets):
     """ counts the number of files in the given subsets
     """
-    return sum(
-        [ilen(os.listdir(
-            os.path.join(kind["corpus"], s))
-        for s in sets)])
+    sets = [os.path.join(kind["corpus"], s) for s in sets]
+    counts = [ilen(os.listdir(s)) for s in sets]
+    return sum(counts)
 
 def get_sample_files(dataset, subset):
     """ Generates a list of data files from the given subset
     """
     for d in dataset:
-        for path in os.path.join(d["corpus"], subset):
-            yield os.path.join(d["corpus"], subset, path)
+        subsetDir = os.path.join(d["corpus"], subset)
+        for path in os.listdir(subsetDir):
+            yield os.path.join(subsetDir, path)
 
 
 def load_fileset(dataset, subset):
 
     counts = [counts_of(kind, subset) for kind in dataset]
-
+    
     data_vector = np.ndarray(
         shape=(sum(counts), 32 * 32),
         dtype=float)
 
-    enumeratedFiles = enumerate(
-        get_sample_files(dataset, subset))
+    samplefiles = list(get_sample_files(dataset, subset))
 
-    for i, imgpath in enumeratedFiles:
-        data_vector[i , :] = imread(imgpath).flatten()
+    for i, imgpath in enumerate(samplefiles):
+        img = imread(imgpath, flatten=True).flatten()
+        data_vector[i , :] = img
 
-    return counts, data_vector
+    return samplefiles, counts, data_vector
 
 def knn(dataSet, testSet):
     """ given a [a x n] dataSet and a [b x n] testSet,
@@ -90,26 +88,26 @@ def knn(dataSet, testSet):
     """
 
     output_indices = np.ndarray(
-        shape=(testSet.shape[0], 1),
+        shape=(testSet.shape[0]),
         dtype=float)
 
     distances = np.ndarray(
-        shape=(dataSet.shape[0], 1),
+        shape=(dataSet.shape[0]),
         dtype=float)
 
-    for testIndex in testSet.shape[0]:
-        for dataIndex in dataSet.shape[0]:
-            distances[dataIndex, 0] = euclidean(
+    for testIndex in range(testSet.shape[0]):
+        for dataIndex in range(dataSet.shape[0]):
+            distances[dataIndex] = euclidean(
                 dataSet[dataIndex, :],
                 testSet[testIndex, :])
 
         # copy the minimum distane index into testIndex
         
-        output[testIndex] = np.argmin(
+        output_indices[testIndex] = np.argmin(
             distances,
             axis=0)
 
-    return output
+    return output_indices
 
 def process_dataset(dataset):
     # check that all the kinds have the right subfolders
@@ -126,33 +124,59 @@ def process_dataset(dataset):
     # in the same numpy array, create a function mapping
     # an index in training to a class
 
-    training_counts, training_set = load_fileset(dataset, "training")
+    training_filenames, training_counts, training_set = (
+        load_fileset(dataset, "training"))
+
     def map_training_index_to_class(input_index):
-        for i, count in enumerated(training_counts):
+        for i, count in enumerate(training_counts):
             input_index -= count
             if input_index <= 0:
                 return dataset[i]
+        print input_index, training_counts
         return None
 
     # get the number of testing files and then put them all
     # in the same numpy array
 
-    testing_counts, testing_set = load_fileset(dataset, "test")
+    testing_filenames, testing_counts, testing_set = (
+        load_fileset(dataset, "test"))
+
     def map_testing_index_to_class(input_index):
-        for i, count in enumerated(testing_counts):
+        for i, count in enumerate(testing_counts):
             input_index -= count
             if input_index <= 0:
-                return i
-        return -1
+                return dataset[i]
+        print input_index, testing_counts
+        return None
 
+    # perform the knn junk
     closests = knn(training_set, testing_set)
-    for testing_index, training_index in enumerated(closests):
-        print("training index = %s, testing_index = %s" % 
-                (i, closest_index))
-        print("training_class =")
-        print map_testing_index_to_class()
-        print map_testing_index_to_class()
 
+    # print the output into a nice csv format
+    # format:   real filename, real name, real gender,                  \ 
+    #           recognized filename, recognized name, recognized gender \
+    #           match on name, match on gender                          \
+
+    for input_index, recognized_index in enumerate(closests):
+        recognized_class = map_training_index_to_class(recognized_index)
+        input_class      = map_testing_index_to_class(input_index)
+
+        if recognized_class == None or input_class == None:
+            print("error mapping indcies back to values")
+            print("    recognized_index = %s" % recognized_index)
+            print("    input_index  = %s" % input_index)
+            sys.exit(1)
+
+        print(", ".join([
+            training_filenames[int(recognized_index)],
+            recognized_class["name"],
+            recognized_class["gender"],
+            testing_filenames[int(input_index)],
+            input_class["name"],
+            input_class["gender"],
+            "1" if input_class["name"] == recognized_class["name"] else "0",
+            "1" if input_class["gender"] == recognized_class["gender"] else "0",
+            ]))
 
 
 
