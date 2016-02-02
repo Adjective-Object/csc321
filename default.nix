7f640dfed9562da5b5986386b340423bec480979
Maxwell <mhuan13@gmail.com>
2016-02-01 20:52:23 -0500

    moved off venv and onto python

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
-    name = "py";
-    buildInputs = [ stdenv python27Full python27Packages.virtualenv libxml2 ];
+    name = "py-321";
+    buildInputs = [ 
+    	stdenv 
+    	python27Full 
+    	python27Packages.virtualenv
+    	python27Packages.numpy
+    	python27Packages.scipy
+    	python27Packages.numpy
+    	python27Packages.pillow
+    	];
     LIBRARY_PATH="${libxml2}/lib";
     shellHook = ''
-      virtualenv --python=python2.7 venv
+      # export "PATH=$PATH:`pwd`/venv/bin/"
+      # virtualenv --python=python2.7 venv
     '';
   };
 }
