6e8d70928049c083da3f15a9fba471488b63bbdc
mhuan13@gmail.com <mhuan13@gmail.com>
2016-03-20 16:55:43 -0500

    visualizing the grad

--- a3/p5/visualize_gradients.py
+++ a3/p5/visualize_gradients.py
@@ -12,19 +12,25 @@ if __name__ == "__main__":
     # if len(sys.argv) != 3:
     #     print "usage: %s <image file> <output directory>" % sys.argv[0]
     
-    img = imread('861.png')
+    img = imread(sys.argv[1])
     img_in = img.reshape(1, 277,277, 3)
-    one_hot = np.array([[np.float32(0) for i in range(6)]])
-    one_hot[0][1] = np.float32(1)
-    p_tensor = sess.run(prob, feed_dict={
-                    x: img_in,
-                    y: one_hot
-                })
-    #this is the problem area
-    dout_dimg = tf.gradients(p_tensor, tf.reshape(x, [-1]))
+
+    radcliffe = np.array([[np.float32(0) for i in range(6)]])
+    radcliffe[0][int(sys.argv[2])] = np.float32(1)
+
+    dout_dimg = tf.gradients(NLL, x) 
+
+    gradient = sess.run(dout_dimg, feed_dict = {
+        x: img_in,
+        y: radcliffe
+    })[0]
+
+    print "min:", np.min(gradient), "max:", np.max(gradient)
+    gradient = (gradient - np.min(gradient)) / (np.max(gradient) - np.min(gradient)) * 255
+    print "min:", np.min(gradient), "max:", np.max(gradient)
 
     imsave(
-        os.path.join(sys.argv[2], "radcliffe_grad.png"),
-        dout_dimg, 
+        os.path.join(sys.argv[3]),
+        gradient.reshape(277,277,3),
         cmap=cm.ocean,
         format="png")
