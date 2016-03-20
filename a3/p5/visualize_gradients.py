9ec4a41f2c6ca317af2c83c65ee200ae665ce5af
Mohdhar Noor <mohdhar.noor@gmail.com>
2016-03-20 11:12:07 -0400

    gradient computation

--- a3/p5/visualize_gradients.py
+++ a3/p5/visualize_gradients.py
@@ -2,6 +2,7 @@ import numpy
 from matplotlib.image import *
 from matplotlib import cm
 import pickle
+import tensorflow as tf
 
 import os, sys
 
@@ -11,12 +12,12 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "usage: %s <image file>" % sys.argv[0]
     
-    image = imread(sys.argv[1]).reshape(1, 277,277, 3)
+    img = imread(sys.argv[1])
+    img_in = img.reshape(1, 277,277, 3)
     one_hot = np.array([[np.float32(0) for i in range(6)]])
     one_hot[0][1] = np.float32(1)
     out_tensor = sess.run(out, feed_dict={
-                    x: image,
+                    x: img_in,
                     y: one_hot
                 })
-    
-    
+    print tf.gradients(out, x)
