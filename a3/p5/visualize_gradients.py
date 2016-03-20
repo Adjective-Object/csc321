c0f2fcfea4a3a565e138b5a71ee8755c9b34cdaa
Mohdhar Noor <mohdhar.noor@gmail.com>
2016-03-20 16:40:53 -0400

    trying to compute softmax tensor gradient

--- a3/p5/visualize_gradients.py
+++ a3/p5/visualize_gradients.py
@@ -9,15 +9,22 @@ import os, sys
 from myalexnet import *
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "usage: %s <image file>" % sys.argv[0]
+    # if len(sys.argv) != 3:
+    #     print "usage: %s <image file> <output directory>" % sys.argv[0]
     
-    img = imread(sys.argv[1])
+    img = imread('861.png')
     img_in = img.reshape(1, 277,277, 3)
     one_hot = np.array([[np.float32(0) for i in range(6)]])
     one_hot[0][1] = np.float32(1)
-    out_tensor = sess.run(out, feed_dict={
+    p_tensor = sess.run(prob, feed_dict={
                     x: img_in,
                     y: one_hot
                 })
-    print tf.gradients(out, x)
+    #this is the problem area
+    dout_dimg = tf.gradients(p_tensor, tf.reshape(x, [-1]))
+
+    imsave(
+        os.path.join(sys.argv[2], "radcliffe_grad.png"),
+        dout_dimg, 
+        cmap=cm.ocean,
+        format="png")
