384faf40f2207912e20b251ef5e88bcaf3068053
Adjective-Object <mhuan13@gmail.com>
2016-04-01 13:28:46 -0400

    added way to visualize weights over generated string

---
+++ a4/p3/p3_overview.py
@@ -0,0 +1,69 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+
+np.random.seed(555)
+random.seed(555)
+
+def normalize(arr):
+   return (arr - arr.min()) / (arr.max() - arr.min())
+
+def inspect(arr, highlight_indexes, threshold=0.7):
+    # create a graph with newline and space highlighted along border
+    arr = normalize(arr)
+    highlighted_numbers = []
+    for x in range(arr.shape[1]):
+        for ind in highlight_indexes:
+            if arr[ind,x] > threshold:
+                print "input %03d -> implies -> output %03d" % (x, ind)
+                highlighted_numbers.append(x)
+
+    arr_graph = np.pad(arr,
+            [(1,0), (1,0)],
+            mode='constant', constant_values=0)
+
+    for ind in highlight_indexes:
+        arr_graph[ind + 1, 0] = 1
+   
+    plt.imshow(arr_graph, cmap=plt.cm.coolwarm, interpolation='nearest')
+    plt.show()
+
+    return highlighted_numbers
+
+
+
+if __name__ == "__main__":
+    # load the file and unpak the weights
+    snapshot = np.load(open("dataset/char-rnn-snapshot.npz"))
+    Wxh = snapshot["Wxh"] 
+    Whh = snapshot["Whh"]
+    Why = snapshot["Why"]
+    bh = snapshot["bh"]
+    by = snapshot["by"]
+
+    mWxh, mWhh, mWhy = snapshot["mWxh"], snapshot["mWhh"], snapshot["mWhy"]
+    mbh, mby = snapshot["mbh"], snapshot["mby"]
+
+    # get other parameters of the matrix
+    chars, data_size, vocab_size, char_to_ix, ix_to_char = [
+        snapshot[x].tolist() for x in [
+            "chars", "data_size", "vocab_size", 
+            "char_to_ix", "ix_to_char"
+        ]]
+
+
+    print "Wxh (looking for things that ':' causes"
+    colon_features = inspect(Wxh.T, [char_to_ix[':']])
+
+    print "Whh.T (looking for how the featues triggered by ':' are pushed through history"
+    next_time_step_colon_features = inspect(Whh.T, colon_features)
+
+    print "Why (looking for features that directly cause newlines and spaces)' '"
+    inds = inspect(Why, [char_to_ix['\n'], char_to_ix[' ']])
+
+    print "features triggered by ':':             ", colon_features
+    print "features trig by ':' on t+1:           ", next_time_step_colon_features
+    print "things that cause newlines and spaces: ", inds
+
+
+
--- a4/p3/p3_visualize.py
+++ a4/p3/p3_visualize.py
@@ -1,34 +1,59 @@
 import numpy as np
+from matplotlib import pyplot as plt
 import random
+import sys
 
 np.random.seed(555)
 random.seed(555)
 
-def activation_map(h, seed_ix, trigger, n):
+
+def color_from_activation(weight):
+    weight = float(weight)
+
+    weight = max(-1, min(1, weight))
+
+    low = [0,0,255]
+    mid = [255,255,255]
+    high = [255, 0, 0]
+
+    mix = ([weight * h + (1 - weight) * m for m,h in zip(mid, high)]
+            if weight > 0 
+            else [-weight * l + (1 + weight) * m for l,m in zip(low, mid)])
+
+    return "#%02x%02x%02x" % tuple(mix)
+
+def visualize_sample(h, neuron, seed_ix, n):
     """ 
     sample a sequence of integers from the model 
     h is memory state, seed_ix is seed letter for first time step
     """
+
     x = np.zeros((vocab_size, 1))
     x[seed_ix] = 1
-    
-    y_neurons = np.empty(by.shape)
-    y_neurons.fill(1);
-
+    ixes = []
     for t in xrange(n):
         h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
         y = np.dot(Why, h) + by
         p = np.exp(y) / np.sum(np.exp(y))
         ix = np.random.choice(range(vocab_size), p=p.ravel())
-
-        if ix == trigger:
-            y_neurons = y_neurons * y
-            y_neurons = y_neurons / y_neurons.max()
-
         x = np.zeros((vocab_size, 1))
         x[ix] = 1
+        ixes.append(ix)
 
-    return y_neurons
+
+        # print as html
+        char = ix_to_char[ix]
+        if char == '\n':
+            char = '&nbsp;<br/>'
+
+        color = color_from_activation(h[neuron] / (h.max() - h.min()))
+        sys.stdout.write(
+            "<span style='background-color: %s;'>%s</span>" % 
+            (color, char))
+
+    return ixes
+
+
 
 if __name__ == "__main__":
     # load the file and unpak the weights
@@ -48,23 +73,10 @@ if __name__ == "__main__":
             "chars", "data_size", "vocab_size", 
             "char_to_ix", "ix_to_char"
         ]]
+    
+    initial_char = random.choice(chars)
+    memory_state = np.zeros(bh.shape)
+    visualize_sample(memory_state, int(sys.argv[1]), char_to_ix[initial_char], 2000)
 
-
-    for temperature in (x / 100.0 for x in range(0,100)):
-        print "######################"
-        print "# TEMPERATUE = %0.5f #" % temperature
-        print "######################"
-
-        for sample_no in range(10):
-            print "## BEGIN SAMPLE %02d" % sample_no
-            # get first char as weighted random choice
-            initial_char = random.choice(chars)
-
-            #initial memstate is null
-            memory_state = np.zeros(bh.shape)
-
-            hallucination = sample(memory_state, char_to_ix[initial_char], 300)
-            print "".join([ix_to_char[index] for index in hallucination])
-            
 
 
